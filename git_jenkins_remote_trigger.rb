5a197d52d1662bab90f24c5d4def66a9e4f44917
James Wang <jameswangz81@gmail.com>
2012-02-23 14:49:40 +0800

    trying to add authentication

--- git_jenkins_remote_trigger.rb
+++ git_jenkins_remote_trigger.rb
@@ -9,6 +9,10 @@ class GitJenkinsRemoteTrigger
 	@@modules = ['api', 'impl']
 	@@poll_internal = 5
 
+	@@auth_required = true
+	# following fileds only required if @@auth_required is true
+	@@api_token = 'dcebe4f09bdc324d2d9567780f04a0c1' 
+		
 	def run
 		while true
 			pull_result = %x[git pull origin master]
@@ -16,11 +20,24 @@ class GitJenkinsRemoteTrigger
 			@@modules.each do |m|
 				result = %x[git log --quiet HEAD~..HEAD #{m}]
 				if not result.empty?
-					puts "triggering job #{m}"
-					Net::HTTP.get_print URI("#{@@jenkins}/job/#{m}/build/")			
+					trigger_job m
 				end
 			end
 			sleep @@poll_internal	
+		end
+	end
+
+	def trigger_job(job_name)
+		puts "triggering job #{job_name}"
+		uri_string = "#{@@jenkins}/job/#{job_name}/build"			
+		if @@auth_required
+			uri = URI("#{uri_string}?token=#{@@api_token}")
+			req = Net::HTTP::Get.new(uri.request_uri)
+			req.basic_auth 'james', 'james'
+			res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
+			puts res.body
+		else
+			Net::HTTP.get_print URI(uri_string) 
 		end
 	end
 
