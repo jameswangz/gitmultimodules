e665b8e25c2cd853ae399f3cb35afb0fb902ba84
James Wang <jameswangz81@gmail.com>
2012-02-23 15:07:15 +0800

    Added authentication

--- git_jenkins_remote_trigger.rb
+++ git_jenkins_remote_trigger.rb
@@ -6,38 +6,38 @@ require 'uri'
 class GitJenkinsRemoteTrigger
 	
 	@@jenkins = 'http://10.30.148.51:8080/jenkins'
-	@@modules = ['api', 'impl']
+	@@modules2jobs = { 'api' => 'api', 'impl' => 'impl' }
 	@@poll_internal = 5
 
 	@@auth_required = true
 	# following fileds only required if @@auth_required is true
+	@@user_name = 'james'
 	@@api_token = 'dcebe4f09bdc324d2d9567780f04a0c1' 
 		
 	def run
 		while true
 			pull_result = %x[git pull origin master]
 			next if pull_result.strip == 'Already up-to-date.'
-			@@modules.each do |m|
-				result = %x[git log --quiet HEAD~..HEAD #{m}]
+			@@modules2jobs.each do |module_name, job_name|
+				result = %x[git log --quiet HEAD~..HEAD #{module_name}]
 				if not result.empty?
-					trigger_job m
+					trigger job_name 
 				end
 			end
 			sleep @@poll_internal	
 		end
 	end
 
-	def trigger_job(job_name)
+	def trigger(job_name)
 		puts "triggering job #{job_name}"
-		uri_string = "#{@@jenkins}/job/#{job_name}/build"			
+		uri = URI("#{@@jenkins}/job/#{job_name}/build")			
 		if @@auth_required
-			uri = URI("#{uri_string}?token=#{@@api_token}")
 			req = Net::HTTP::Get.new(uri.request_uri)
-			req.basic_auth 'james', 'james'
-			res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
+			req.basic_auth @@user_name, @@api_token
+			res = Net::HTTP.start(uri.host, uri.port) { |http| http.request(req) }
 			puts res.body
 		else
-			Net::HTTP.get_print URI(uri_string) 
+			Net::HTTP.get_print uri 
 		end
 	end
 
