6cf93d6d04afc2d375570deea02f2ec85acdbb7c
James Wang <jameswangz81@gmail.com>
2012-03-28 15:46:57 +0800

    changed jenkins server

--- jenkins_trigger.rb
+++ jenkins_trigger.rb
@@ -3,7 +3,8 @@
 require './git_jenkins_remote_trigger'
 
 if __FILE__ == $0
-	jenkins = 'http://localhost:8081/jenkins'
+	#jenkins = 'http://localhost:8080/jenkins'
+	jenkins = 'http://localhost:8081'
 	module_job_mappings = { 'api' => 'api', 'impl' => 'impl' }
 	running_options = { :only_once => false, :interval => 5 }
 	auth_options = { :required => false, :username => 'james', :api_token => 'dcebe4f09bdc324d2d9567780f04a0c1' }
